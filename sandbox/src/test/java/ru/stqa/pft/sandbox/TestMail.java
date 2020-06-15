0a22229f1c8c7901f9fedee1b84c777c3d5616eb
ADmitriev99 <66620372+ADmitriev99@users.noreply.github.com>
2020-06-15 19:06:13 +0300

    проверка работоспособности теста mailru

--- sandbox/src/test/java/ru/stqa/pft/sandbox/TestMail.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/TestMail.java
@@ -3,21 +3,35 @@ package ru.stqa.pft.sandbox;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
     public class TestMail {
-        @Test public void login() { // TODO Auto-generated method stub
+        @Test public void login() throws InterruptedException { // TODO Auto-generated method stub
             System.setProperty("webdriver.chrome.driver", "a:\\Distrib\\chromedriver\\chromedriver.exe");
             WebDriver driver=new ChromeDriver();
             //driver.manage().window().maximize();
             driver.get("https://mail.ru/");
+            //System.out.println("1");
             driver.findElement(By.id("mailbox:login")).click();
+            //System.out.println("2");
             driver.findElement(By.id("mailbox:login")).sendKeys("test.test.8989");
-            driver.findElement(By.cssSelector(".mailbox__row_")).click();
-            driver.findElement(By.cssSelector(".mailbox__icon_next path")).click();
-            driver.findElement(By.id("mailbox:password")).click();
-            driver.findElement(By.id("mailbox:password")).sendKeys("1PassTes");
+            //System.out.println("3");
+            driver.findElement(By.cssSelector(".mailbox__icon_next > svg")).click();
+            //System.out.println("4");
+            //driver.findElement(By.id("mailbox:password")).click();
+            //System.out.println("5");
+            //driver.findElement(By.id("mailbox:password")).sendKeys("1PassTes");
+            //System.out.println("6");
             driver.findElement(By.cssSelector("#mailbox\\3Asubmit > .o-control")).click();
+            //System.out.println("7");
+            driver.findElement(By.id("mailbox:password")).sendKeys("1PassTes");
+            //System.out.println("8");
+            driver.findElement(By.cssSelector("#mailbox\\3Asubmit > .o-control")).click();
+            Thread.sleep(2000);
+            System.out.println(driver.getCurrentUrl());
 
+            String Wanted = "https://e.mail.ru/inbox/?back=1&afterReload=1";
 
+            Assert.assertEquals(driver.getCurrentUrl(), Wanted);
         }
     }
