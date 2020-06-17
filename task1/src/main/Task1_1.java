57ab3fb8d6c8dba8fcfd877a778c7d40d72a112b
ADmitriev99 <66620372+ADmitriev99@users.noreply.github.com>
2020-06-17 22:56:59 +0300

    убрал .idea

--- task1/src/main/Task1_1.java
+++ task1/src/main/Task1_1.java
@@ -2,11 +2,11 @@ package main;
 
 public class Task1_1 {
     public static void main(String[] args) {
-        System.out.println(sleepIn(true, true));
+        System.out.println(sleepIn(true, false));
     }
     public static boolean sleepIn(boolean weekday, boolean vacation)
     {
-        if ((weekday==true) && (vacation=false))
+        if ((weekday==true) && (vacation==false))
             return false;
         else
             return true;
